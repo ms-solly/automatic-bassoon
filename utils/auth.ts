fc14968092cdb79f7e51306b006fef7e8bc95626
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-09-16 02:31:14 +0530

    Update auth.ts

--- utils/auth.ts
+++ utils/auth.ts
@@ -11,7 +11,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `env.siteUrl`, 
+      redirectTo: `https://automatic-bassoon-rho.vercel.app`, 
     },
   });
 
